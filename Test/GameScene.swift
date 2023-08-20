5cb9a43ec1538e964ee7792ba942d966b0031c52
Kryštof Svejkovský <svejkovsky.kry@gmail.com>
2023-08-21 00:44:07 +0200

    base scructure of the code

--- Test/GameScene.swift
+++ Test/GameScene.swift
@@ -15,41 +15,91 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
         spawnObstacles()
     }
     
+    struct PhysicsCategories {
+        static let dinosaur: UInt32 = 0x1 << 1
+        static let obstacle: UInt32 = 0x1 << 2
+        static let ground: UInt32 = 0x1 << 3
+    }
+    
     func setupDinosaur() {
         dinosaur = SKSpriteNode(imageNamed: "dinosaur")
-        // set dinosaur properties and add to scene
+        
+        dinosaur.position = CGPoint(x: dinosaur.size.width/2, y: ground.size.height + dinosaur.size.height/2)
+        
+        dinosaur.physicsBody = SKPhysicsBody(rectangleOf: dinosaur.size)
+        dinosaur.physicsBody?.isDynamic = true
+        dinosaur.physicsBody?.categoryBitMask = PhysicsCategories.dinosaur
+        dinosaur.physicsBody?.collisionBitMask = PhysicsCategories.ground | PhysicsCategories.obstacle
+        dinosaur.physicsBody?.contactTestBitMask = PhysicsCategories.obstacle
+        
         addChild(dinosaur)
     }
     
     func setupGround() {
         ground = SKSpriteNode(imageNamed: "ground")
-        // set ground properties, physics, and add to scene
+        ground.position = CGPoint(x: size.width / 2, y: ground.size.height / 2)
+        ground.physicsBody = SKPhysicsBody(rectangleOf: ground.size)
+        ground.physicsBody?.isDynamic = false
+        ground.physicsBody?.categoryBitMask = PhysicsCategories.ground
         addChild(ground)
+        
     }
-    
+
     func setupScore() {
         scoreLabel = SKLabelNode(fontNamed: "Arial")
-        // set label properties and add to scene
         addChild(scoreLabel)
     }
     
-    func spawnObstacles() {
-        // Spawn obstacles at intervals and set them to move from right to left
+    func updateScore() {
+        score += 1
+        scoreLabel.text = "Score: \(score)"
     }
+    
+    func spawnObstacles() {
+        let createObstacle = SKAction.run {
+            let obstacle = SKSpriteNode(imageNamed: "obstacleImageName")
+            obstacle.position = CGPoint(x: self.size.width + obstacle.size.width / 2, y: self.ground.size.height + obstacle.size.height / 2)
+            obstacle.physicsBody = SKPhysicsBody(rectangleOf: obstacle.size)
+            obstacle.physicsBody?.categoryBitMask = PhysicsCategories.obstacle
+            obstacle.physicsBody?.isDynamic = false
+            
+            let moveAction = SKAction.moveTo(x: -obstacle.size.width, duration: 5)
+            let removeAction = SKAction.removeFromParent()
+            obstacle.run(SKAction.sequence([moveAction, removeAction]))
+            
+            self.addChild(obstacle)
+        }
+        
+        let waitAction = SKAction.wait(forDuration: 2)
+        let spawnSequence = SKAction.sequence([createObstacle, waitAction])
+        let spawnForever = SKAction.repeatForever(spawnSequence)
+        run(spawnForever)
+    }
+
     
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
         dinosaurJump()
     }
     
     func dinosaurJump() {
-        // Apply force or change velocity to make dinosaur jump
+        let jumpForce = CGVector(dx: 0, dy: 200)
+        dinosaur.physicsBody?.applyImpulse(jumpForce)
     }
     
+    func didBegin(_ contact: SKPhysicsContact) {
+        if (contact.bodyA.categoryBitMask == PhysicsCategories.dinosaur && contact.bodyB.categoryBitMask == PhysicsCategories.obstacle) ||
+           (contact.bodyB.categoryBitMask == PhysicsCategories.dinosaur && contact.bodyA.categoryBitMask == PhysicsCategories.obstacle) {
+            gameOver()
+        }
+    }
+
+    func startGame() {
+        score = 0
+        self.scene?.isPaused = false
+    }
+
     func gameOver() {
-        // Handle game over logic
+        self.scene?.isPaused = true
     }
-    
-    func restartGame() {
-        // Reset the game
-    }
+
 }
